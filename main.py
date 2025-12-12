e2d0e4b6e57e9bebfd611569c3bea246b4ea2dca
William Levine <23029518+william-levine@users.noreply.github.com>
2025-12-12 02:47:58 +0000

    implemented main functionality

---
+++ main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import time
+
+CA_SIZE = 71 # including left and right edge boundaries
+ITERATION_COUNT = 1000
+DELAY_DURATION = 0.05 # seconds
+
+def displayPattern(generation):
+    """Converts a CA generation array (consisting of 0s and 1s) to a pattern of cells and displays it"""
+    gen_pattern = np.copy(generation)
+    gen_pattern[generation == "0"] = " "
+    gen_pattern[generation == "1"] = "■"
+    print("".join(gen_pattern[1:len(generation)-1])) # doesn't display the boundary cells
+
+def main():
+    initial_generation = np.full(shape = CA_SIZE, dtype=str, fill_value=0)
+    initial_generation[int(CA_SIZE / 2)] = 1
+
+    current_generation = initial_generation
+    next_generation = np.full(shape = CA_SIZE, dtype=str, fill_value=0)
+
+    print("Wolfram Rule: ")
+    wolfram_rule = int(input())
+    wolfram_rule_binary = '{0:08b}'.format(wolfram_rule)
+
+    ruleset = { "111": wolfram_rule_binary[0],
+                "110": wolfram_rule_binary[1],
+                "101": wolfram_rule_binary[2],
+                "100": wolfram_rule_binary[3],
+                "011": wolfram_rule_binary[4],
+                "010": wolfram_rule_binary[5],
+                "001": wolfram_rule_binary[6],  
+                "000": wolfram_rule_binary[7]}
+
+    for n in range(ITERATION_COUNT):
+        displayPattern(current_generation)
+        time.sleep(DELAY_DURATION)
+        next_generation = np.full(shape = CA_SIZE, dtype=str, fill_value=0)
+        for i in range(1, CA_SIZE-1):
+            neighbourhood = "".join(current_generation[i-1:i+2])
+            new_state = ruleset[neighbourhood]
+            next_generation[i] = new_state
+        current_generation = next_generation
+
+
+if __name__ == "__main__":
+    main()
